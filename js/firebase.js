36eb95a6555ec2d2c9c36cf5d454b48b146c1803
Miketryartd <142714918+Miketryartd@users.noreply.github.com>
2024-04-05 14:10:48 +0800

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAtn_TqmNrkgVVxaUaiLsyZxsYu9JfhKsU",
+    authDomain: "quickie-2145b.firebaseapp.com",
+    projectId: "quickie-2145b",
+    storageBucket: "quickie-2145b.appspot.com",
+    messagingSenderId: "798851286165",
+    appId: "1:798851286165:web:5153786e3654bbd998c79c",
+    measurementId: "G-FBW668JLLD"
+  };
+firebase.initializeApp(firebaseConfig);
+  // Initialize Firebase  
